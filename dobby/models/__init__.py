7baf9893765fe106e799d68b10bcdebabb4f6492
Simon Berger <siku2@outlook.com>
2018-08-01 10:44:40 +0200

    improved converters

--- dobby/models/__init__.py
+++ dobby/models/__init__.py
@@ -1,10 +1,10 @@
 from .calendar import Calendar
 from .context import Context
-from .converter import Converter
+from .converter import Converter, converter
 from .group import Group, GroupMixin
 from .job import Job
 from .report import Report
 from .slave import Slave, slave
 from .task import Task
 
-__all__ = ["Calendar", "Context", "Converter", "Group", "GroupMixin", "Job", "Report", "Slave", "slave", "Task"]
+__all__ = ["Calendar", "Context", "Converter", "converter", "Group", "GroupMixin", "Job", "Report", "Slave", "slave", "Task"]
--- dobby/models/converter.py
+++ dobby/models/converter.py
@@ -1,15 +1,22 @@
 import abc
+import importlib
 import inspect
 import logging
-from typing import Any, Union
+from typing import Any, Callable, Mapping, Type, Union
 
-from ..config import DictContainer, ListContainer
 from ..errors import ConversionError
 
 log = logging.getLogger(__name__)
 
+CONVERTER_MAP = {}
+
 
 class Converter(abc.ABC):
+    options: Mapping
+
+    def __init__(self, **kwargs):
+        self.options = kwargs
+
     def __repr__(self) -> str:
         sig = inspect.signature(self.convert)
         return_hint = sig.return_annotation
@@ -24,31 +31,58 @@ class Converter(abc.ABC):
         pass
 
 
-class DictConverter(Converter):
-    def convert(self, arg: Any, **kwargs) -> dict:
-        if isinstance(arg, DictContainer):
-            return arg.to_normal()
-        return dict(arg)
+class FuncConverter(Converter):
+    def __init__(self, func: Callable, **kwargs):
+        self.func = func
+        func_sig = inspect.signature(func)
+
+        self.convert.__annotations__.update({
+            "arg": func_sig.parameters["arg"].annotation,
+            "return": func_sig.return_annotation
+        })
+
+        super().__init__(**kwargs)
+
+    def convert(self, arg: Any, **kwargs) -> Any:
+        return self.func(arg, **kwargs)
 
 
-class ListConverter(Converter):
-    def convert(self, arg: Any, **kwargs) -> list:
-        if isinstance(arg, ListContainer):
-            return arg.to_normal()
-        return list(arg)
+def _class_converter(cls: Type[Converter], *targets: Any, **kwargs) -> Converter:
+    conv = cls(**kwargs)
+    return conv
 
 
-CONVERTER_MAP = {
-    dict: DictConverter,
-    list: ListConverter
-}
+def _func_converter(func: Callable, *targets: Any, **kwargs) -> Converter:
+    conv = FuncConverter(func, **kwargs)
+    return conv
 
 
-def convert(converter, arg, **kwargs):
-    origin = getattr(converter, "__origin__", False)
+def converter(*targets: Any, **kwargs):
+    for target in targets:
+        if not inspect.isclass(target):
+            raise TypeError(f"targets must be a tuple of types, not {target}")
+
+    def decorator(arg):
+        if inspect.isclass(arg) and issubclass(arg, Converter):
+            _converter = _class_converter(arg, *targets, **kwargs)
+        else:
+            _converter = _func_converter(arg, *targets, **kwargs)
+
+        for target in targets:
+            if target in CONVERTER_MAP:
+                raise KeyError(f"There's already a converter for {target}")
+            CONVERTER_MAP[target] = _converter
+
+        return _converter
+
+    return decorator
+
+
+def convert(_converter, arg, **kwargs):
+    origin = getattr(_converter, "__origin__", False)
     if origin:
         if origin is Union:
-            types = getattr(converter, "__args__")
+            types = getattr(_converter, "__args__")
             if isinstance(arg, types):
                 return arg
             else:
@@ -62,24 +96,27 @@ def convert(converter, arg, **kwargs):
                         e.__cause__ = last_exc
                         last_exc = e
                         log.debug(f"Couldn't coerce {arg!r} to {_type}")
-                    raise ConversionError(f"Couldn't convert {arg!r} to any of {converter}", value=arg, converter=converter) from last_exc
+                    raise ConversionError(f"Couldn't convert {arg!r} to any of {_converter}", value=arg, converter=_converter) from last_exc
 
-    if converter in CONVERTER_MAP:
-        converter = CONVERTER_MAP[converter]
+    if _converter in CONVERTER_MAP:
+        _converter = CONVERTER_MAP[_converter]
 
     try:
-        if inspect.isclass(converter):
-            if issubclass(converter, Converter):
-                inst = converter()
+        if inspect.isclass(_converter):
+            if issubclass(_converter, Converter):
+                inst = _converter()
                 return inst.convert(arg, **kwargs)
-            elif hasattr(converter, "convert") and inspect.ismethod(converter.convert):
-                return converter.convert(arg, **kwargs)
-        elif isinstance(converter, Converter):
-            return converter.convert(arg, **kwargs)
+            elif hasattr(_converter, "convert") and inspect.ismethod(_converter.convert):
+                return _converter.convert(arg, **kwargs)
+        elif isinstance(_converter, Converter):
+            return _converter.convert(arg, **kwargs)
 
-        return converter(arg)
+        return _converter(arg)
     except ConversionError:
         raise
     except Exception as e:
-        raise ConversionError(f"Couldn't convert {arg!r} using {converter}", value=arg, converter=converter,
+        raise ConversionError(f"Couldn't convert {arg!r} using {_converter}", value=arg, converter=_converter,
                               hint="Make sure that you're passing a valid value for the parameter \"{self.key}\"") from e
+
+
+importlib.import_module("._builtin_converters", __package__)
